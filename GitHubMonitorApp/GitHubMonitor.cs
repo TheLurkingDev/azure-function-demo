879958a43338eceac08649cdaa0bb789b512cda7
Darren Cook <thelurkingdev@gmail.com>
2019-12-13 16:31:52 -0500

    Removed 'get' request from function. Logged request body.

--- GitHubMonitorApp/GitHubMonitor.cs
+++ GitHubMonitorApp/GitHubMonitor.cs
@@ -14,20 +14,17 @@ namespace GitHubMonitorApp
     {
         [FunctionName("GitHubMonitor")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("GitHub Monitor processed an action.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            log.LogInformation(requestBody);
+
+            return new OkResult();
         }
     }
 }
